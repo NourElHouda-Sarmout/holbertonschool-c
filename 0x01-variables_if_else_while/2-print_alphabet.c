c386e7fec7a177d6402ed6b46f6bdbefe5254a9c
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-11 20:11:36 +0000

    Solve Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Solve Task 2 */
+
+/**
+ *main- print the alphabet in lowercase
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+return (0);
+}
