1fc37287f803011ab52973497a63d4620c7cd24c
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-11 20:42:58 +0000

    Solve Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* Solve Task 5 */
+
+/**
+ *main- prints all single digit numbers
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+		printf("%d", i);
+	putchar('\n');
+
+	return (0);
+}
