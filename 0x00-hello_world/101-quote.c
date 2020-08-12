7d921952048a8ab402b04714d9a53923b7a48039
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-12 18:35:38 +0000

    Solve Task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main- prints to standard error
+ *Return: 1 (Error)
+ */
+int main(void)
+{
+printf("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+
+return (1);
+}
