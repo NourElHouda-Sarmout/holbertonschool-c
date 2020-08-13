530a516d256067df81029548103b2bbef99e0313
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-13 20:12:40 +0000

    Solve Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * times_table -  prints 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int mul;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			mul = i * j;
+
+			if ((mul > 9 && mul < 100) || (mul < -9 && mul > -100))
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+			else
+			{
+				_putchar(mul + '0');
+			}
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
