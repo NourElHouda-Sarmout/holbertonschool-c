2d597e1798fa6542f6550dbb4b5d58e9f6da9505
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-13 19:28:12 +0000

    Solve Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * print_to_98 -  prints from n to 98
+ *@n: integer
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+int i;
+
+if (n == 98)
+	printf("%d" , n);
+
+if (n < 98)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d" , i);
+if (i != 98)
+{
+putchar(',');
+putchar(' ');
+}
+
+}
+}
+
+if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d" , i);
+if (i != 98)
+{
+putchar(',');
+putchar(' ');
+}
+
+}
+}
+
+putchar('\n');
+}
