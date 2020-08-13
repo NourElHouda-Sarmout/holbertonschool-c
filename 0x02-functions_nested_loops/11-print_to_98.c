8b924add6ca352e5eafc07bd15b241f5b7dad53c
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-13 19:31:13 +0000

    Solve Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,13 @@ void print_to_98(int n)
 int i;
 
 if (n == 98)
-	printf("%d" , n);
+	printf("%d", n);
 
 if (n < 98)
 {
 for (i = n; i <= 98; i++)
 {
-printf("%d" , i);
+printf("%d", i);
 if (i != 98)
 {
 putchar(',');
@@ -30,7 +30,7 @@ if (n > 98)
 {
 for (i = n; i >= 98; i--)
 {
-printf("%d" , i);
+printf("%d", i);
 if (i != 98)
 {
 putchar(',');
