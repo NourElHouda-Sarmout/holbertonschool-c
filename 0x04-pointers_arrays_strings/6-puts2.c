9864b30a250c03e86de554183a1cba3ceaee787c
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-17 18:13:34 +0000

    Solve Task 6

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *puts2 - prints every other character of a string
+ *@str: a pointer the string we want to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	char *c;
+
+	for (c = str; *c; c += 2)
+	{
+		_putchar(*c);
+	}
+
+	_putchar('\n');
+
+}
