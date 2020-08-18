6d0d5e885de1398651956fc3ae68a5e0f4c1fbd0
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-18 11:20:22 +0000

    Solve Task 2

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	if (n > 0)
+		dest[n - 1] = '\0';
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
 
-	return (dest);
 
+
+	return (dest);
 }
