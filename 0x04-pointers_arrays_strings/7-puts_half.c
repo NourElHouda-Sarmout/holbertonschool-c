79f6beadb6cc9b00c9968a965a336f2264d4872d
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-19 14:26:41 +0000

    Fix Task 7

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  *puts_half - prints half of a string
  *@str: a pointer the string we want to print its half
@@ -8,37 +7,17 @@
  */
 void puts_half(char *str)
 {
-	int length, c;
+	int a;
 
-	length = string_length(str);
+	for (a = 0; str[a] != '\0'; a++)
+		;
+	a++;
 
-	if (length % 2 == 0)
+	for (a /= 2; str[a] != '\0'; a++)
 	{
-		for (c = length / 2 ; c < length ; c++)
-			_putchar(str[c]);
-	}
-	else
-	{
-		for (c = (length - 1) / 2 ; c < length ; c++)
-			_putchar(str[c]);
+		_putchar(str[a]);
 	}
 
 	_putchar('\n');
 
-}
-
-/**
- *string_length - determine the length of a string
- *@s: a pointer the string we want to know its length
- *
- * Return: int (length of a string)
- */
-int string_length(char *s)
-{
-	int c = 0;
-
-	while (*(s + c) != '\0')
-		c++;
-
-	return (c);
 }
