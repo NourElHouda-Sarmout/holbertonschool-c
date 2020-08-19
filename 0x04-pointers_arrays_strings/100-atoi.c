93e265c0e18423dd39c9021cbbca37d5db364785
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-08-19 15:19:49 +0000

    Fix Task 100

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,48 +1,27 @@
 #include "holberton.h"
-/**
- * _isdigit - Check for digits (0 through 9)
- * @c: int type param
- * Return: 1 if digit, else 0
- */
-
-int _isdigit(int c)
-{
-	if (c > 47 && c < 58)
-		return (1);
-
-	return (0);
-}
-
 
 /**
- * _atoi - program to converst ASCII to integer
- * @s: pointer to array of character
- * Return: Always successful
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
  */
-
 int _atoi(char *s)
 {
-	int digit;
-	int count = 0;
 	int sign = 1;
-	unsigned int value = 0;
-	unsigned int pos = 1;
+	unsigned int num = 0;
 
-	for (digit = 0; ((s[digit] < '0') || (s[digit] > '9')); digit++)
-	{
-		if (s[digit] == '-')
+	do {
+		if (*s == '-')
 			sign *= -1;
-	}
-	for (; _isdigit(s[digit]); digit++)
-	{
-		count++;
-	}
-	while (count > 0)
-	{
-		value += ((s[digit - 1] - 48) * pos);
-		pos *= 10;
-		count--;
-		digit--;
-	}
-	return (value * sign);
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
