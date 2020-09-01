5a7eab20ffa83595b337699a9526197da46840af
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-09-01 19:49:24 +0000

    Re-Solve Task 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -4,19 +4,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
+	int i, name_len = 0, owner_len = 0;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
 	newDog = malloc(sizeof(dog_t));
 	if (newDog == NULL)
-		return NULL;
+		return (NULL);
 
-	newDog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	for (i = 0; name[i]; i++)
+		name_len++;
+
+	for (i = 0; owner[i]; i++)
+		owner_len++;
+
+	newDog->name = malloc(sizeof(char) * (name_len + 1));
 	if (newDog->name == NULL)
 	{
 		free(newDog);
 		return (NULL);
 	}
 
-	newDog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	newDog->owner = malloc(sizeof(char) * (owner_len + 1));
 	if (newDog->owner == NULL)
 	{
 		free(newDog->name);
